ada1b690661b39e86601672bc7c6b03e3bb2dbdc
NimniSenarathna <nimni.20210008@iit.ac.lk>
2024-04-03 18:12:55 +0530

    added component into main

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,14 @@
 // pages/index.js
 import React from 'react';
 import Products from '../app/pages/components/Products'
+import ShoppingCart from '../app/pages/components/ShoppingCart'
+
 const Home = () => {
   return (
     <div className="container mx-auto py-8">
       <h1 className="text-3xl font-semibold mb-4">Products</h1>
       <Products />
+      <ShoppingCart />
     </div>
   );
 };
