f3ed7e78011e22742dc8ad7751f0e5e83d2b5a05
Anupama <anupama@bistecglobal.com>
2024-04-03 17:08:49 +0530

    changes

---
+++ src/app/pages/index.jsx
@@ -0,0 +1,14 @@
+// pages/index.js
+import React from 'react';
+import Products from '../components/Products';
+
+const Home = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-3xl font-semibold mb-4">Products</h1>
+      <Products />
+    </div>
+  );
+};
+
+export default Home;
