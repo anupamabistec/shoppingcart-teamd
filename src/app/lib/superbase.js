4c710fe391cd58a6a39aa118691f4b03eb1f4de9
Anupama <anupama@bistecglobal.com>
2024-04-03 15:57:18 +0530

    changes

--- src/app/lib/superbase.js
+++ src/app/lib/superbase.js
@@ -1,7 +1,5 @@
 // lib/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hleajrvjcqpwviinsdmy.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsZWFqcnZqY3Fwd3ZpaW5zZG15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxMzQ3NjQsImV4cCI6MjAyNzcxMDc2NH0.9lAMemyYsD1FSTv7EVM2ZbBnl0KVyzxuJDiU4hpYt38';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
